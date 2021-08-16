5c7f6c678d647711552dea77a3c3a5387cf3ba87
dullface <samil_taramali@hotmail.com>
2021-08-16 21:28:15 +0300

    zaman

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { float } from 'getenv';
+import { left } from 'inquirer/lib/utils/readline';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Avatar } from "react-native-elements";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>kek</Text>
+      <View style={styles.headerIngame}>
+        <Text style={styles.welcomeText}>Hail my king!</Text>
+        <View style={styles.headerInline}>
+          <Text style={styles.flagsLeft}>
+            40
+          </Text>
+          <Avatar style={styles.avatarPerson} rounded title="MD" />
+          <Text style={styles.timePast}>
+            00
+          </Text>
+        </View>
+      </View>
+      <View style={styles.bodyIngame}>
+        <Text>Fail my king!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +32,70 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  headerIngame: {
+    flexDirection: 'column',
+    color: 'red',
+    width: '100%',
+    height: '40%',
+    maxHeight: '40%',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'red',
+  },
+  headerInline: {
+    flexDirection: 'column',
+  },
+  welcomeText: {
+    marginTop: '7%',
+    top: '25px',
+    marginHorizontal: '15%',
+    paddingVertical: '.5rem',
+    textAlign: 'center',
+    fontSize: '1rem',
+    fontWeight: '600',
+    color: 'red',
+    backgroundColor: 'black',
+  },
+  bodyIngame: {
+    color: 'blue',
+    width: '100%',
+    height: '60%',
+    maxHeight: '60%',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'blue',
+  },
+  avatarPerson: {
+
+  },
+  timePast: {
+    position: 'absolute',
+    width: '17%',
+    paddingVertical: '.5rem',
+    left: '15%',
+    textAlign: 'center',
+    marginTop: '7%',
+    fontSize: '1rem',
+    fontWeight: '600',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'red',
+    color: 'red',
+    backgroundColor: 'black',
+  },
+  flagsLeft: {
+    position: 'absolute',
+    width: '17%',
+    paddingVertical: '.5rem',
+    right: '15%',
+    textAlign: 'center',
+    marginTop: '7%',
+    fontSize: '1rem',
+    fontWeight: '600',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'red',
+    color: 'red',
+    backgroundColor: 'black',
+  },
+  headerInline: {
+    flexDirection: 'row',
   },
 });
